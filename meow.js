164c9573f09f73ec1ec46c891a661da2df82d039
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2022-04-17 19:26:58 +0300

    Spawn recording process in next tick on event-loop

--- meow.js
+++ meow.js
@@ -44,60 +44,62 @@ const createWindow = () => {
 const startRecording = () => {
     console.log('startRecording()');
 
-    recProc = execFile(path.resolve(path.join(__dirname, 'bin/ffmpeg.exe')), [
-        '-y',
-        '-f',
-        'gdigrab',
-        '-framerate',
-        '10',
-        '-offset_x',
-        win.getPosition()[0],
-        '-offset_y',
-        win.getPosition()[1],
-        '-video_size',
-        win.getSize().join('x'),
-        '-show_region',
-        '1',
-        '-i',
-        'desktop',
-        '-filter_complex',
-        '[0:v] fps=10,split [a][b];[a] palettegen=max_colors=20:reserve_transparent=0:stats_mode=single [p];[b][p] paletteuse=new=1',
-        temp_gif_path
-    ]);
-
-    is_recording = true;
-
-    recProc.on('exit', (code, signal) => {
-        spawnSync(
-            path.resolve(path.join(__dirname, 'bin/gifsicle.exe')),
-            [
-                '-O3',
-                temp_gif_path,
-                '-o',
-                temp_gif_path
-            ]
-        );
-        
-        const save_path = dialog.showSaveDialogSync(win, {
-            title: 'Save GIF to',
-            filters: [{name: 'GIF', extensions: ['gif']}]
-        });
-
-        if (save_path) {
-            fs.copyFileSync(temp_gif_path, save_path);
-            spawn('explorer.exe', ['/select,', save_path], {
-                detached: true,
-                stdio: ['ignore']
-            });
-        }
-
-        fs.unlinkSync(temp_gif_path);
-
-        recProc.kill('SIGTERM');
-        win.focus();
-    });
-
     win.minimize();
+
+    setTimeout(() => {
+        recProc = execFile(path.resolve(path.join(__dirname, 'bin/ffmpeg.exe')), [
+            '-y',
+            '-f',
+            'gdigrab',
+            '-framerate',
+            '10',
+            '-offset_x',
+            win.getPosition()[0],
+            '-offset_y',
+            win.getPosition()[1],
+            '-video_size',
+            win.getSize().join('x'),
+            '-show_region',
+            '1',
+            '-i',
+            'desktop',
+            '-filter_complex',
+            '[0:v] fps=10,split [a][b];[a] palettegen=max_colors=20:reserve_transparent=0:stats_mode=single [p];[b][p] paletteuse=new=1',
+            temp_gif_path
+        ]);
+    
+        is_recording = true;
+    
+        recProc.on('exit', (code, signal) => {
+            spawnSync(
+                path.resolve(path.join(__dirname, 'bin/gifsicle.exe')),
+                [
+                    '-O3',
+                    temp_gif_path,
+                    '-o',
+                    temp_gif_path
+                ]
+            );
+            
+            const save_path = dialog.showSaveDialogSync(win, {
+                title: 'Save GIF to',
+                filters: [{name: 'GIF', extensions: ['gif']}]
+            });
+    
+            if (save_path) {
+                fs.copyFileSync(temp_gif_path, save_path);
+                spawn('explorer.exe', ['/select,', save_path], {
+                    detached: true,
+                    stdio: ['ignore']
+                });
+            }
+    
+            fs.unlinkSync(temp_gif_path);
+    
+            recProc.kill('SIGTERM');
+            win.focus();
+        });
+    }, 100);
 };
 
 const stopRecording = () => {
